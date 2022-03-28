736314858f1c4032df3270f3df87ebc292d3f294
MarceloFontana <100136439+MarceloFontana@users.noreply.github.com>
2022-03-28 15:22:49 -0300

    Projeto Final

---
+++ app/src/main/java/com/samuel/interconection/receive.kt
@@ -0,0 +1,11 @@
+package com.samuel.interconection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class receive : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.receive)
+    }
+}
